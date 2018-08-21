807e3ef1f764d14a09cf00943fecc2d7bbde7c6a
vladimiriii <brett.romero@hotmail.com>
2018-08-21 22:56:05 +0200

    Layout changes, select statement processing.

--- app/static/script/index.js
+++ app/static/script/index.js
@@ -4,5 +4,77 @@
 On page ready
 -------------------------------------*/
 $(document).ready(function(){
-	console.log("Ready to rock and/or roll!")
+
+	$('#submitButton').click(function() {
+		const sqlInput = $('#sqlInput').val();
+
+		const queryComponents = processSQL(sqlInput);
+
+	});
 });
+
+// Splits and processes SQL
+function processSQL(query) {
+
+	// Remove comments
+	query = query.replace(/(\/\*.*\*\/)/g, '').replace(/--.*/g, '');
+
+	// Remove multiple spaces and leading/trailing whitespace
+	query = query.replace(/\s\s+/g, ' ').replace(/^\s+|\s+$/g, '');
+
+	// Split into components
+	rawColumns = query
+		.substring(0, query.toLowerCase().indexOf('from'))
+		.substring(query.toLowerCase().indexOf(' '))
+		.split(",");
+
+	columns = [];
+	aliases = [];
+	for (colIdx in rawColumns) {
+		row = rawColumns[colIdx].replace(/^\s+|\s+$/g, ''); // trim whitespace from each row
+
+		if (row.toLowerCase().indexOf(" as ") >= 0) {
+			columns[colIdx] = row.substring(0, row.indexOf(" "));
+			aliases[colIdx] = row.substring(row.toLowerCase().indexOf(" as ") + 4);
+		}
+		else if (row.toLowerCase().indexOf(" ") >= 0) {
+			columns[colIdx] = row.substring(0, row.indexOf(" "));
+			aliases[colIdx] = row.substring(row.toLowerCase().indexOf(" ") + 1);
+		}
+		else {
+			columns[colIdx] = row.replace(/^\s+|\s+$/g, '');
+			aliases[colIdx] = null;
+		};
+	};
+
+	// Generate columns and rename clause
+	columnClause = '[["' + columns.join('", "') + '"]]';
+	renameClause = ".rename(columns={";
+	for (idx in aliases) {
+		if (aliases[idx] != null) {
+			renameClause += '"' + columns[idx] + '": "' + aliases[idx] + '", ';
+		};
+	};
+	renameClause = renameClause.substring(0, renameClause.length - 2) + "})";
+
+	console.log(columnClause + renameClause);
+
+	// fromStatement = query.substring(query.toLowerCase().indexOf('from'), query.toLowerCase().indexOf('where'));
+	// whereStatement = query.substring(query.toLowerCase().indexOf('where'), query.length);
+	//
+	// // Standardize string and split into an array
+	// // query = query.match(/[^\r\n]+/g);
+	// console.log(selectStatement);
+	// console.log(fromStatement);
+	// console.log(whereStatement);
+
+	// // Determine is SELECT or WITH query
+	// firstCommand = query[0].toLowerCase();
+	//
+	// // If SELECT statement first
+	// if (firstCommand == 'select') {
+	//
+	// }
+
+
+}
---
+++ app/static/script/sqlSyntax.js
@@ -0,0 +1,43 @@
+const sqlSyntax = {
+	"select": [
+		"select",
+		"case",
+		"when",
+		"then"
+		"else",
+		"end",
+		"nullif",
+		"ifnull",
+		"cast",
+		"as"
+	],
+	"from": [
+		"from",
+		"join",
+		"inner",
+		"left",
+		"right",
+		"full",
+		"outer",
+		"cross",
+		"using",
+		"on",
+		"and"
+	],
+	"where": [
+		"where"
+	],
+	"boolean": [
+		"between"
+		"and",
+		"or",
+		"in",
+		"=",
+		">",
+		"<",
+		">=",
+		"<=",
+		"!=",
+		"<>"
+	]
+}
